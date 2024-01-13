141b930b15947d7ec2546b430995a0a2db0de80c
16bravo <c.bravo2@yahoo.fr>
2024-01-14 00:28:15 +0100

    Update elo_scrapping.py

--- elo_scrapping.py
+++ elo_scrapping.py
@@ -5,8 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-import undetected_chromedriver as uc
-print(uc.Patcher.data_path)
 
 chrome_path = './chrome-driver/chromedriver'
 driver = webdriver.Chrome(service=ChromeService(executable_path=chrome_path))
